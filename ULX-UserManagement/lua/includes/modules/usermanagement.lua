a85488b519a9c28413678110f2597fd2962222dc
Christian Deacon <gamemann@gflclan.com>
2017-06-21 09:21:46 -0400

    Completely Recoded For MySQLoo

--- ULX-UserManagement/lua/includes/modules/usermanagement.lua
+++ ULX-UserManagement/lua/includes/modules/usermanagement.lua
@@ -1,18 +1,21 @@
-require("tmysql4");
-
---[[ Database. ]]--
-local Database, lastError = ""; 
+require("mysqloo");
 
 --[[ Predefined functions ]]--
 local error, ErrorNoHalt, GetConVarNumber, GetConVarString, Msg, pairs, print, ServerLog, tonumber, tostring, tobool, unpack =
-      error, ErrorNoHalt, GetConVarNumber, GetConVarString, Msg, pairs, print, ServerLog, tonumber, tostring, tobool, unpack ;
+      error, ErrorNoHalt, GetConVarNumber, GetConVarString, Msg, pairs, print, ServerLog, tonumber, tostring, tobool, unpack;
 
-local concommand, game, hook, math, os, player, string, table, timer, mysqloo, gatekeeper =
-      concommand, game, hook, math, os, player, string, table, timer, mysqloo, gatekeeper ;
+local concommand, game, hook, math, os, player, string, table, timer, mysqloo, gatekeeper, IsValid =
+      concommand, game, hook, math, os, player, string, table, timer, mysqloo, gatekeeper, IsValid;
 	  
 local tmysql, PrintTable, file = tmysql, PrintTable, file;
 
-local function RecreateConnection() end
+
+--[[ Database. ]]--
+local Db
+local bActive = false
+
+--[[ Query functions. ]]--
+local queryLoadUser
 
 module("usermanagement");
 
@@ -22,180 +25,25 @@ local config =
     username = "root";
     password = "";
     database = "usermanagement";
-	group1 = "Member";
-	group2 = "Supporter";
-	group3 = "VIP";
-	debugToFile = true;
-	debugNotificationsFile = "notifications.txt";
-	debugErrorsFile = "errors.txt";
+	port = 3306;
+	groups = {};
+	excludeAdmins = true;
 	debug = true;
-	retryTimer = true;
-	retryTimerValue = 300;
-	refreshTimer = 300;
-};
-
---[[ Tables ]]--
-local users;
-
-local queries = 
-{
-	["LoadUser"] = "SELECT `groupid` FROM `sb_perks` WHERE `steamid`='%s'"
+	debugDebugFile = "notifications.txt";
+	debugErrorsFile = "errors.txt";
 };
 
 --[[ Convenience Functions ]]--
-local function notifyerror(...)
-    ErrorNoHalt("[", os.date(), "][UserManagement.lua] ", ...);
-    ErrorNoHalt("\n");
-    print();
-	
-	if (config.debugToFile and config.debug) then
-		local words = table.concat({"[",os.date(),"][UserManagement.lua] ",...},"").."\n";
+local function printMsg(sFile, sMsg)
+	local words = table.concat({"[UM] ", sMsg}, "") .. "\n";
+	print(words)
 		
-		-- Create direction if it doesn't exist.
-		if (not file.Exists("usermanagement/", "DATA")) then
-			file.CreateDir("usermanagement");
-		end
-		
-		file.Append("usermanagement/" .. config.debugErrorsFile, words);
-	end
-end
-
-local function notifymessage(...)
-    local words = table.concat({"[",os.date(),"][UserManagement.lua] ",...},"").."\n";
-    print(words);
-	
-	if (config.debugToFile and config.debug) then
-		-- Create direction if it doesn't exist.
-		if (not file.Exists("usermanagement/", "DATA")) then
-			file.CreateDir("usermanagement");
-		end
-
-		file.Append("usermanagement/" .. config.debugNotificationsFile, words);
+	-- Create direction if it doesn't exist.
+	if (not file.Exists("usermanagement/", "DATA")) then
+		file.CreateDir("usermanagement");
 	end
 	
-end
-
---[[ Query Functions ]]--
-local loadUser;
-
-local function loadUserCallback(ply, results)
-	if (results == nil or not ply or not ply:IsPlayer() or not results[1].data[1]) then
-		if (config.debug) then
-			notifymessage("[GFL-UserManagement]loadUserCallback() :: Results are nil for \"" .. ply:SteamID64() .. "\".");
-		end
-			
-		return;
-	end
-	
-	local groupID = results[1].data[1].groupid;
-	local sGroup = ply:GetUserGroup();
-	
-	if (sGroup == config.group1 or sGroup == config.group2 or sGroup == config.group3 or sGroup == "user") then
-		if (groupID == 1) then
-			-- Member.
-			ply:SetUserGroup(config.group1);
-		elseif (groupID == 2) then
-			-- Supporter.
-			ply:SetUserGroup(config.group2);
-		elseif (groupID == 3) then
-			-- VIP.
-			ply:SetUserGroup(config.group3);
-		end
-		
-		if (config.debug) then
-			notifymessage("[GFL-UserManagement]loadUserCallback() :: User added with group ID " .. groupID .. " (" .. ply:SteamID64() .. ")");
-		end
-	else
-		--[[ If they already have a custom group, go away! ]]--
-		if (config.debug) then
-			notifymessage("[GFL-UserManagement]loadUserCallback() :: User \"" .. ply:SteamID64() .. "\" (" .. ply:SteamID() .. ") is already in a group (" .. ply:GetUserGroup() .. ")! Aborting!");
-		end
-		
-		return;
-	end
-end
-
--- Functions
-function loadUser(ply)
-	if (not ply or not ply:IsPlayer() or not Database or not Database:IsConnected()) then
-		if (config.debug) then
-			notifyerror("[GFL-UserManagement]loadUser() :: Connection not valid... Error: " .. lastError);
-		end
-		
-		return;
-	end
-	
-	if (config.debug) then
-		notifymessage("[GFL-UserManagement]loadUser() :: " .. ply:Nick() .. " (\"" .. ply:SteamID64() .. "\") authed!");
-	end
-	
-	local sSteamIDEscaped = Database:Escape(ply:SteamID64());
-	local sQuery = queries["LoadUser"]:format(sSteamIDEscaped);
-	
-	Database:Query(sQuery, loadUserCallback, ply, false);
-end
-
-function loadUsers()
-	for _,v in pairs (player.GetAll()) do
-		loadUser(v);
-	end
-end
-
---[[ Hooks ]]--
-do
-    local function ShutDown()
-        if Database then
-			Database:Disconnect();
-		end
-	end
-
-	local function PlayerInitialSpawn( ply )
-		loadUser(ply)
-    end
-	
-    hook.Add("ShutDown", "UserManagement.lua - ShutDown", ShutDown);
-    hook.Add("PlayerInitialSpawn", "UserManagement.lua - PlayerInitialSpawn", PlayerInitialSpawn);
-end
-
--- Keep the MySQL open.
-local function KeepMySQLOpen()
-	if (config.debug) then
-		notifymessage("[GFL-UserManagement]KeepMySQLOpen() :: Checking database connection to ensure it is opened.");
-	end
-	
-	if (not Database or not Database:IsConnected()) then
-		if (config.debug) then
-			notifyerror("[GFL-UserManagement]KeepMySQLOpen() :: Found the Database down. Reconnecting!");
-		end
-		
-		Activate(true);
-	end
-end	
-
----
--- Starts the database and activates the module's functionality.
-function Activate(bRetry)
-	Database, lastError = tmysql.initialize(config.hostname, config.username, config.password, config.database, 3306, nil, CLIENT_MULTI_STATEMENTS);
-	
-	notifymessage("[GFL-UserManagement]Activate() :: Connecting to " .. config.hostname .. "!");
-	
-	if (not Database or not Database:IsConnected()) then
-		notifyerror("[GFL-UserManagement]Activate() :: Error connecting to MySQL server. Error: " .. lastError);
-	else
-		notifymessage("[GFL-UserManagement]Activate() :: Successfully connected!");
-		
-		if (bRetry) then
-			loadUsers();
-		end
-	end
-	
-	if (config.retryTimer) then
-		if (timer.Exists("UM_KeepMySQLOpen")) then
-			timer.Remove("UM_KeepMySQLOpen");
-		end
-		
-		timer.Create("UM_KeepMySQLOpen", config.retryTimerValue, 0, KeepMySQLOpen);
-	end
+	file.Append("usermanagement/" .. sFile, "[" .. os.date() .. "] " .. words);
 end
 
 function SetConfig(key, value)
@@ -206,60 +54,106 @@ function SetConfig(key, value)
     config[key] = value;
 end
 
-concommand.Add("um_reloadusers", function(ply, cmd)
-    if (ply:IsPlayer() and ply:IsAdmin()) then
-		notifymessage("[GFL-UserManagement]um_reloadusers() :: Executed by " .. ply:Nick() .. " (" .. ply:SteamID() .. ").");
-		loadUsers();
-		ply:ChatPrint("[GFL-UserManagement]Users reloaded.");
-	else
-		if (not ply:IsPlayer()) then
-			loadUsers();
-			print("[GFL-UserManagement]Users reloaded. [CONSOLE]");
-		else
-			notifymessage("[GFL-UserManagement]um_reloadusers() :: " .. ply:SteamID() .. " attempted \"um_reloadusers\" but is not an admin.");
-			ply:ChatPrint("[GFL-UserManagement]You do not have access to this command.");
-		end
-	end
-end, nil, "Reloads all users.");
+--[[ Queries ]] --
+local arrQueries = 
+{
+	["LoadUser"] = "SELECT `groupid` FROM `sb_perks` WHERE `steamid`='%s'"
+}
 
-concommand.Add("um_restart", function (ply, cmd)
-	if (ply:IsPlayer()) then
-		return;
-	end
+local function ConnectToDB()
+	Db = mysqloo.connect(config.hostname, config.username, config.password, config.database, config.port)
 	
-	RecreateConnection();
-end);
-
-concommand.Add("um_checkconnection", function (ply, cmd)
-	if (Database and Database:IsConnected()) then
-		print("connected..");
-	else
-		print("not connected");
-	end
-end);
-
--- Completely restart the MySQL connection for this addon.
-local function RecreateConnection()
-	if (config.debug) then
-		notifymessage("[GFL-UserManagement]RecreateConnection() :: Recreating database connection!");
-	end
+	Db:setAutoReconnect(true)
 	
-	if (Database and Database:IsConnected()) then
-		if (config.debug) then
-			notifymessage("[GFL-UserManagement]RecreateConnection() :: Database active, releasing it!");
-		end
+	--[[ Database connection functions ]]--
+	function Db:onConnected()
+		bActive = true
 		
-		Database:Disconnect();
+		if config.debug then
+			printMsg(config.debugDebugFile, "Successfully connected to the database!")
+		end
+	end
+
+	function Db:onConnectionFailed(sErr)
+		printMsg(config.debugErrorsFile, "Error connecting to the database. User Management not active. Error: " .. sErr)
+		bActive = false
 	end
 	
-	Activate(true);
+	-- Connect to the database!
+	Db:connect()
 end
 
--- Refresh timer
-timer.Create("um_refreshtimer", config.refreshTimer, 0, function()
+function Activate()
+	ConnectToDB()
+end
+
+local function loadUser(ply)
+	if not ply or not bActive then return end
+	
+	if config.excludeAdmins and ply:IsAdmin() then return end
+	
+	local sSteamID = ply:SteamID64()
+	local sLookUp = Db:escape(sSteamID)
+	
+	local sqlStr = arrQueries["LoadUser"]:format(sLookUp)
+	
+	queryLoadUser = Db:query(sqlStr)
+	
+	function queryLoadUser:onError(sErr, sSQL)
+		printMsg(config.debugErrorsFile, "Error executing query: \"" .. sSQL .. "\". Error: " .. sErr)
+	end
+
+	function queryLoadUser:onData(data)
+		if not data then return end
+		
+		if config.debug then
+			printMsg(config.debugDebugFile, "Loading user " .. ply:Nick() .. " (" .. sSteamID .. ") with group '" .. config.groups[data["groupid"]] .. "' (" .. data["groupid"] .. ")!")
+		end
+		
+		ply:SetUserGroup(config.groups[data["groupid"]])
+	end
+	
+	queryLoadUser:start()
+end
+
+local function loadUsers()
+	for k,v in pairs(player.GetAll()) do
+		loadUser(v)
+	end
+end
+
+hook.Add("PlayerInitialSpawn", "UM_PlayerInitialSpawn", function (ply)
+	loadUser(ply)
+end)
+
+concommand.Add("um_reloadusers", function(ply)
+	if IsValid(ply) and not ply:IsSuperAdmin() then
+		ply:ChatPrint("[UM] You must be a Super Admin to run this command!")
+		return
+	end
+	
 	loadUsers()
 	
-	if (config.debug) then
-		notifymessage("[GFL-UserManagement]um_refreshtimer() :: Refresh timer ran")
+	if IsValid(ply) then
+		ply:ChatPrint("[UM] All users reloaded.")
+	else
+		print("[UM] All users reloaded.")
+	end
+end)
+
+concommand.Add("um_restart", function(ply)
+	if IsValid(ply) and not ply:IsSuperAdmin() then
+		ply:ChatPrint("[UM] You must be a Super Admin to run this command!")
+		return
+	end
+	
+	Db:disconnect(true)
+	
+	ConnectToDB()
+	
+	if IsValid(ply) then
+		ply:ChatPrint("[UM] Attempting to restart User Management.")
+	else
+		print("[UM] Attempting to restart User Management.")
 	end
 end)
--- ULX-UserManagement/lua/ulx/modules/usermanagement_init.lua
+++ ULX-UserManagement/lua/ulx/modules/usermanagement_init.lua
@@ -1,21 +1,23 @@
 require("usermanagement")
-usermanagement.SetConfig("hostname", "myHost.com");
+usermanagement.SetConfig("hostname", "myHost");
 usermanagement.SetConfig("username", "myUser");
 usermanagement.SetConfig("password", "myPass");
 usermanagement.SetConfig("database", "myDatabase");
+usermanagement.SetConfig("port", 3306);
 
--- Only three groups supported. Edit this if you absolutely need to edit the group names (e.g. if they have to be lower-case or something). I would prefer keeping it as it is.
-usermanagement.SetConfig("group1", "member");
-usermanagement.SetConfig("group2", "supporter");
-usermanagement.SetConfig("group3", "vip");
+-- Groups (ID => Name)
+local groups = 
+{
+	[1] = "member",
+	[2] = "supporter",
+	[3] = "vip"
+}
+usermanagement.SetConfig("groups", groups);
+usermanagement.SetConfig("excludeAdmins", true)
 
 -- Other options
-usermanagement.SetConfig("debug", false);
-usermanagement.SetConfig("debugToFile", true);
-usermanagement.SetConfig("debugNotificationsFile", "notifications.txt");
+usermanagement.SetConfig("debug", true);
+usermanagement.SetConfig("debugDebugFile", "debug.txt");
 usermanagement.SetConfig("debugErrorsFile", "errors.txt");
-usermanagement.SetConfig("retryTimer", false);
-usermanagement.SetConfig("retryTimerValue", 300);
-usermanagement.SetConfig("refreshTimer", 300);
 
-usermanagement.Activate(false);
+usermanagement.Activate();
